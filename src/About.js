1fe2d58cc56683ac907844070b66c975940dca7a
Alex Truxess <alexm00@vt.edu>
2023-12-27 15:08:27 -0500

    forgot to update new track

--- src/About.js
+++ src/About.js
@@ -12,11 +12,11 @@ function About() {
                     I received my Bachelor's degree in Computer Science December 2022 and I'm currently pursuing my Master of Engineering from Virginia Tech. I plan to graduate December 2024.
                 </p>
                 <p className="chicago" style={{marginRight: "3%", marginLeft: "3%"}}>
-                    The focus for my master is Human-Computer Interaction. I have an interest in front-end development and design, as well as 3D animation and video editing. 
+                    The focus for my masters is Human-Computer Interaction and Software Development & Applications. I have an interest in front-end development and design, as well as 3D animation and video editing. 
                 </p>
                 <h2 className="chicago" style={{marginRight: "62%", color: "#63D471"}}>My Interests</h2>
                 <div className="chicago" style={{marginRight: "50%", marginLeft: "3%"}}>
-                    I am a movie buff and I have a physical disc collection of over 600 DVDs and Blu-rays.
+                    I am a movie buff and I have a physical disc collection of roughly 700 DVDs and Blu-rays.
                 </div>
                 <div className="chicago" style={{marginLeft: "50%", marginRight: "3%"}}>
                     I received my 4th degree black belt at the age of 15.
