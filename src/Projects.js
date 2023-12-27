c6120ed4bd3b29ea39845a35f3f6c3737f922cd3
Alex Truxess <alexm00@vt.edu>
2023-12-27 15:02:55 -0500

    update for Parkopedia

--- src/Projects.js
+++ src/Projects.js
@@ -9,6 +9,26 @@ function Projects() {
         <div>
             <header>
                 <h1 className="chicago" style={{color: "#63D471"}}>Projects</h1>
+                <h2 className="chicago" style={{marginRight: "50%", marginLeft: "3%", color: "#63D471"}}>Smart Parking App Prototype</h2>
+                <p className="chicago" style={{marginRight: "40%", marginLeft: "3%"}}>
+                    For my Models and Theories of Human-Computer Interaction course, I developed a high fidelity Figma app for my team which would serve as a smart parking service at Virginia Tech.
+                </p>
+                <p className="chicago" style={{marginLeft: "40%", marginRight: "3%"}}>
+                    It allows users to determine which parking spots are open in different parking lots, and navigate to their selected locations. There is also the option to navigate back to your spot when users return to their vehicles.
+                </p>
+                <p className="chicago" style={{marginRight: "40%", marginLeft: "3%"}}>
+                    Users can also utilize the application for paying their tickets, and parking passes would be used in coordination with gates to eliminate the need for parking officers. 
+                </p>
+                <p>
+                        <a
+                            className="App-link"
+                            href="https://www.figma.com/proto/qxD2aut2j5SZQTUZyI9FDi/Parkopedia?type=design&node-id=201-2040&t=eVCs2wPC3Ybp6A5Z-1&scaling=min-zoom&page-id=201%3A1100&starting-point-node-id=201%3A2040&show-proto-sidebar=1&mode=design"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            >
+                            <div className="chicago" style={{marginLeft: "40%", marginRight: "3%"}}>Parkopedia Figma App</div>
+                        </a>
+                    </p>
                 <h2 className="chicago" style={{marginRight: "50%", marginLeft: "3%", color: "#63D471"}}>Virtual Art Gallery</h2>
                 <p className="chicago" style={{marginRight: "40%", marginLeft: "3%"}}>
                     For my Human-Computer Interaction capstone for my Bachelor's degree, I assisted in working on a virtual art gallery within the metaverse using a 3D model of the Moss Arts Center at Virginia Tech.
@@ -42,7 +62,7 @@ function Projects() {
                 <p className="chicago" style={{marginLeft: "40%", marginRight: "3%"}}>
                     I worked on the back-end of the project, where I built an API with python. I then connected that API to a database using DynamoDB so that the front end could reach the data.
                 </p>
-                <h2 className="chicago" style={{marginRight: "50%", marginLeft: "3%", color: "#63D471"}}>Figma Project</h2>
+                <h2 className="chicago" style={{marginRight: "50%", marginLeft: "3%", color: "#63D471"}}>Streaming Service App Prototype</h2>
                 <p className="chicago" style={{marginRight: "40%", marginLeft: "3%"}}>
                     For my Intro to Human-Computer interaction course our final project was a design for a streaming service that could access all other services. This way, you could access all of your subscriptions from one place.
                 </p>
